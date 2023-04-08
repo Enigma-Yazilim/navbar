5bcba1bd673d6de291c1f982322acb77ef4a7a7c
Berk Bıçakcı <120296952+bicakciberk@users.noreply.github.com>
2023-04-08 10:08:37 +0300

    Add files via upload

--- navbar/app.js
+++ navbar/app.js
@@ -1,13 +1,8 @@
-const hamburgerOpen = document.querySelector('.open');
-const hamburgerClose = document.querySelector('.close');
 const menu = document.querySelector('.nav-links');
-const hamburger = document.querySelectorAll('.hamburger');
+const hamburger = document.querySelector('.hamburger');
+const checkbox = document.querySelector('#checkbox')
 
-hamburger.forEach((hamburger) => {
-    hamburger.addEventListener('click',function(){
-        menu.classList.toggle('active');
-        hamburgerOpen.classList.toggle('remove');
-        hamburgerClose.classList.toggle('active')
-    });
-});
+checkbox.addEventListener('click',function(){
+    menu.classList.toggle('active')
+})
 
